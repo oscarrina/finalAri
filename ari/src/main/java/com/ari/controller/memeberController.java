983bcb8d6848d97a377a045c9d2f3056c0154563
BOOK-QBKFS2P52O\jiyeo <ssy9453@naver.com>
2023-09-27 11:15:50 +0900

    윤지영 - 로그인, 아이디찾기, 비밀번호 재설정 페이지 css

--- ari/src/main/java/com/ari/controller/memeberController.java
+++ ari/src/main/java/com/ari/controller/memeberController.java
@@ -15,4 +15,28 @@ public class memeberController {
 		return "memberJoin/memberJoin";
 	}
 
+	@RequestMapping("/memberLogin")
+	public String memberLogin() {
+		return "member/memberLogin";
+	}
+	
+	@RequestMapping("/idFind")
+	public String idFind() {
+		return "member/idFind";
+	}
+	
+	@RequestMapping("/pwdFind1")
+	public String pwdFind1() {
+		return "member/pwdFind1";
+	}
+	
+	@RequestMapping("/pwdFind2")
+	public String pwdFind2() {
+		return "member/pwdFind2";
+	}
+	
+	@RequestMapping("/pwdFind3")
+	public String pwdFind3() {
+		return "member/pwdFind3";
+	}
 }
