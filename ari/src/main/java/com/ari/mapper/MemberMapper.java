b8a4cfa2b358383527e8a0f2e12054d624e3c365
songjun <song_jun0@naver.com>
2023-10-04 17:54:17 +0900

    카카오 소셜 로그인

--- ari/src/main/java/com/ari/mapper/MemberMapper.java
+++ ari/src/main/java/com/ari/mapper/MemberMapper.java
@@ -8,5 +8,8 @@ public interface MemberMapper {
 	public MemberDTO getUserInfo(String userid);
 	public MemberDTO idFind(String username, String usertel);
 	public boolean idCheck(String userid);
+	public int kakaoCheck(String userid);
+	public int kakaoJoin(MemberDTO dto);
+	public String kakaoToken(String userid);
 
 }
