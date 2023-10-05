3bc4bd17036e99fe17f8f71cfa08401dbcee3227
DESKTOP-ELNG3TP\woo <owlman124@gmail.com>
2023-10-05 09:43:33 +0900

    1005 우선철 1차

--- ari/src/main/java/com/ari/mapper/MemberMapper.java
+++ ari/src/main/java/com/ari/mapper/MemberMapper.java
@@ -7,7 +7,7 @@ public interface MemberMapper {
 	public String memberLogin(String userid);
 	public MemberDTO getUserInfo(String userid);
 	public MemberDTO idFind(String username, String usertel);
-	public boolean idCheck(String userid);
+	public int idCheck(String userid);
 	public int kakaoCheck(String userid);
 	public int kakaoJoin(MemberDTO dto);
 	public String kakaoToken(String userid);
